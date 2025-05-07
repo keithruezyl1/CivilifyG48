f7e24402e4a1fc6f65748da1783dd1cdab8022f5
bardatt1 <westley.bus1@gmail.com>
2025-05-08 02:13:37 +0800

    for-testing merging

--- backend/src/main/java/com/capstone/civilify/entity/Role.java
+++
@@ -1,6 +0,0 @@
-package com.capstone.civilify.entity;
-
-public enum Role {
-    USER,
-    ADMIN
-}
--- backend/src/main/java/com/capstone/civilify/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capstone.civilify.repository;
-
-import com.capstone.civilify.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByEmail(String email);
-}
